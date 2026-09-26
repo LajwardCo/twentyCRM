dc89793f895b57491ad38e0f20f7c7fb27980ed6
rashid2003 <rashid.obaidi2003@gmail.com>
2026-09-26 13:51:08 +0430

    chore(sales-app): per-area survey stylesheets

--- packages/twenty-sales-app/src/main.tsx
+++ packages/twenty-sales-app/src/main.tsx
@@ -7,6 +7,10 @@ import { App } from './App';
 import { registerServiceWorker } from './lib/pwa';
 import './styles.css';
 import './surveys.css';
+import './styles/surveys-builder.css';
+import './styles/surveys-collect.css';
+import './styles/surveys-responses.css';
+import './styles/surveys-campaigns.css';
 
 registerServiceWorker();
 
